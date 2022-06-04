0fce58cdb89164d9ed3febef7897148bcf29e407
GreenAvocados <chimp5967@outlook.com>
2022-06-03 20:08:57 -0500

    Added BootStrap stuff I think idk?

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,27 @@
+// import * as bootstrap from "bootstrap";
+// import "bootstrap";
+// import "bootstrap/dist/css/bootstrap.css"; // Import precompiled Bootstrap css
+// import "@fortawesome/fontawesome-free/css/all.css";
+
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css"; // Import precompiled Bootstrap css
+import "@fortawesome/fontawesome-free/css/all.css";
+import "./offcanvas.css";
+import Holder from "holderjs";
+
+$(() => {
+  "use strict";
+
+  $('[data-toggle="offcanvas"]').on("click", function () {
+    $(".offcanvas-collapse").toggleClass("open");
+  });
+});
+
+Holder.addTheme("thumb", {
+  bg: "#55595c",
+  fg: "#eceeef",
+  text: "Thumbnail",
+});
+
 // alert("Thanks for visiting!");
